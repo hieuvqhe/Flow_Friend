99d013c3b72ea2e7b640e6b115fa3a5bc0f2fd2e
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-03-07 21:04:17 +0700

    feat: add like tweet logic

---
+++ server/src/services/like.services.ts
@@ -0,0 +1,29 @@
+import { Like } from '~/models/schemas/Like.schema'
+import databaseService from './database.services'
+import { ObjectId } from 'mongodb'
+import { LIKES_MESSAGE } from '~/constants/messages'
+
+class LikesTweet {
+  async likeTweet(user_id: string, tweet_id: string) {
+    const existsLike = await databaseService.likes.findOne({
+      user_id: new ObjectId(user_id),
+      tweet_id: new ObjectId(tweet_id)
+    })
+    if (!existsLike) {
+      const result = await databaseService.likes.insertOne(
+        new Like({
+          user_id: new ObjectId(user_id),
+          tweet_id: new ObjectId(tweet_id)
+        })
+      )
+      return { message: LIKES_MESSAGE.LIKES_TWEET_SUCCESS, result: result }
+    } else {
+      return { message: LIKES_MESSAGE.ALREADY_LIKED_THIS_TWEET }
+    }
+  }
+  
+  
+}
+
+const likesTweet = new LikesTweet()
+export default likesTweet
