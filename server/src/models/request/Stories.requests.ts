ac96d922e6278eab11a918487da2316b6396a998
chinhpxhhe171177 <chinhpxhe171177@fpt.edu.vn>
2025-03-11 13:56:42 +0700

    stories models

---
+++ server/src/models/request/Stories.requests.ts
@@ -0,0 +1,17 @@
+import { MediaTypeQuery } from '~/constants/enums'
+
+export interface createNewStoryResBody {
+    content: string
+    media_url: string
+    media_type: MediaTypeQuery
+    caption: string
+    privacy: string[]
+}
+export interface viewAndStatusStoryResBody {
+    story_id: string
+    content: string
+    view_status: string
+}
+export interface ReactStoryResBody {
+    reaction_type: string
+}
