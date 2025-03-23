d7836d52800f0d1a8608d4547162e91790005d93
chinhpxhhe171177 <chinhpxhe171177@fpt.edu.vn>
2025-03-23 09:27:54 +0700

    story commit

---
+++ client/src/components/Navigation/SideBarLink.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export const SideBarLink = ({ Icon, text, path }) => {
+    return (
+        <Link to={path} className='text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation'>
+            <div>{Icon}</div>
+            <span className="hidden xl:inline">{text}</span>
+        </Link>
+    )
+}
